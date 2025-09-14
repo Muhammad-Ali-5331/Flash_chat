356620fd115f88b2c033d1e2f6035c56bf34a6e3
Muhammad Ali <malitariq5324@gmail.com>
2025-09-14 13:11:50 +0500

    - Added loading animation when user presses registration button.
    - Toggled off the input keyboard when Register button is pressed.

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -3,6 +3,7 @@ import 'package:flash_chat/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
 import '../components/rounded_button.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class RegistrationScreen extends StatefulWidget {
   const RegistrationScreen({super.key});
@@ -14,69 +15,84 @@ class RegistrationScreen extends StatefulWidget {
 class RegistrationScreenState extends State<RegistrationScreen> {
   late String email;
   late String password;
+  bool _isloading = false;
   final _authorizer = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: 'logo',
-              child: SizedBox(
-                height: 200.0,
-                child: Image.asset('images/logo.png'),
+      body: ModalProgressHUD(
+        inAsyncCall: _isloading,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Hero(
+                tag: 'logo',
+                child: SizedBox(
+                  height: 200.0,
+                  child: Image.asset('images/logo.png'),
+                ),
               ),
-            ),
-            SizedBox(height: 48.0),
-            TextField(
-              keyboardType: TextInputType.emailAddress,
-              style: TextStyle(color: Colors.black),
-              textAlign: TextAlign.center,
-              onChanged: (value) {
-                //Email Entry Field
-                email = value;
-              },
-              decoration: kTextFieldDecoration.copyWith(
-                hintText: 'Enter your email',
+              SizedBox(height: 48.0),
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                style: TextStyle(color: Colors.black),
+                textAlign: TextAlign.center,
+                onChanged: (value) {
+                  //Email Entry Field
+                  email = value;
+                },
+                decoration: kTextFieldDecoration.copyWith(
+                  hintText: 'Enter your email',
+                ),
               ),
-            ),
-            SizedBox(height: 8.0),
-            TextField(
-              style: TextStyle(color: Colors.black),
-              textAlign: TextAlign.center,
-              obscureText: true,
-              onChanged: (value) {
-                //Password Entry Field
-                password = value;
-              },
-              decoration: kTextFieldDecoration.copyWith(
-                hintText: 'Enter your password',
+              SizedBox(height: 8.0),
+              TextField(
+                style: TextStyle(color: Colors.black),
+                textAlign: TextAlign.center,
+                obscureText: true,
+                onChanged: (value) {
+                  //Password Entry Field
+                  password = value;
+                },
+                decoration: kTextFieldDecoration.copyWith(
+                  hintText: 'Enter your password',
+                ),
               ),
-            ),
-            SizedBox(height: 24.0),
-            RoundedButton(Colors.blueAccent, 'Register', () async {
-              try {
-                final userCredentials = await _authorizer
-                    .createUserWithEmailAndPassword(
-                      email: email,
-                      password: password,
-                    );
-                final newUser = userCredentials.user;
-                if (newUser != null) {
-                  Navigator.pushNamed(context, ChatScreen.id);
+              SizedBox(height: 24.0),
+              RoundedButton(Colors.blueAccent, 'Register', () async {
+                FocusScope.of(context).unfocus();
+                await Future.delayed(Duration(milliseconds: 300));
+                setState(() {
+                  _isloading = true;
+                });
+                try {
+                  final userCredentials = await _authorizer
+                      .createUserWithEmailAndPassword(
+                        email: email,
+                        password: password,
+                      );
+                  final newUser = userCredentials.user;
+                  if (newUser != null) {
+                    setState(() {
+                      _isloading = false;
+                    });
+                    Navigator.pushNamed(context, ChatScreen.id);
+                  }
+                  setState(() {
+                    _isloading = false;
+                  });
+                } catch (e) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Registration failed: $e')),
+                  );
                 }
-              } catch (e) {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(content: Text('Registration failed: $e')),
-                );
-              }
-            }),
-          ],
+              }),
+            ],
+          ),
         ),
       ),
     );
