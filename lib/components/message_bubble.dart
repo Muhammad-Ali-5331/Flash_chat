363f2998c97b478deaf82e969c56451bfb09b7a2
Muhammad Ali <malitariq5324@gmail.com>
2025-09-27 11:08:11 +0500

    Enhance MessageBubble: Add isMe flag for styling and sender labeling - Added `isMe` property to differentiate between current user and others - Adjusted alignment: messages from self align left, others align right - Updated label to show "You" for self messages - Applied distinct colors and border radius styles using constants.dart - Reduced elevation for a more balanced look

--- lib/components/message_bubble.dart
+++ lib/components/message_bubble.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flash_chat/constants.dart';
 
 class MessageBubble extends StatelessWidget {
   final String text;
   final String sender;
   final String time;
+  final bool isMe;
   const MessageBubble({
     super.key,
     required this.sender,
     required this.text,
     required this.time,
+    required this.isMe,
   });
 
   @override
@@ -16,13 +19,17 @@ class MessageBubble extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(10.0),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
+        crossAxisAlignment: isMe
+            ? CrossAxisAlignment.start
+            : CrossAxisAlignment.end,
         children: [
-          Text('$sender ($time)', style: TextStyle(fontSize: 12.0)),
+          isMe
+              ? Text('You ($time)', style: TextStyle(fontSize: 12.0))
+              : Text('$sender ($time)', style: TextStyle(fontSize: 12.0)),
           Material(
-            elevation: 10.0,
-            color: Colors.blueAccent,
-            borderRadius: BorderRadius.circular(30.0),
+            elevation: 8.0,
+            color: isMe ? Colors.grey : Colors.blueAccent,
+            borderRadius: isMe ? isMeStyle : isNotMeStyle,
             child: Padding(
               padding: const EdgeInsets.symmetric(
                 vertical: 10.0,
